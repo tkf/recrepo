b74a4ca4b3337ca8f1a7a0a1d30a114e7b6921c6
Takafumi Arakaki <aka.tkf@gmail.com>
2019-01-30 17:53:37 -0800

    Initial implementation

--- recrepo.py
+++ recrepo.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 """
+Record repository status after asserting it's clean.
 """
 
 # Copyright 2019, Takafumi Arakaki
@@ -27,3 +28,140 @@
 __version__ = "0.0.0"
 __author__ = "Takafumi Arakaki"
 __license__ = "MIT"
+
+from collections import namedtuple
+from io import StringIO
+from pathlib import Path
+import json
+import subprocess
+import sys
+
+
+RepoState = namedtuple("RepoState", ["is_clean", "status", "revision", "toplevel"])
+
+
+def to_record(state):
+    r = state._asdict()
+    del r["status"]
+    return r
+
+
+def repostate(pointer):
+    path = Path(pointer).resolve()
+    if not path.is_dir():
+        path = path.parent
+
+    def git(*args):
+        return subprocess.check_output(
+            ["git"] + list(args), universal_newlines=True, cwd=str(path)
+        )
+
+    status = git("status", "--short")
+    return RepoState(
+        is_clean=not status.strip(),
+        status=status,
+        revision=git("rev-parse", "HEAD").strip(),
+        toplevel=git("rev-parse", "--show-toplevel").rstrip(),
+    )
+
+
+class DirtyRepositories(Exception):
+
+    CODE = 113
+    """ Exit status when there is an un-clean repository. """
+    # Using 113 and below as it seems that's for "users". See:
+    # https://www.tldp.org/LDP/abs/html/exitcodes.html
+
+    def __init__(self, repos):
+        self.repos = repos
+
+    def __str__(self):
+        repos = [r for r in self.repos if not r.is_clean]
+        io = StringIO()
+
+        def _print(*args, **kwargs):
+            print(*args, file=io, **kwargs)
+
+        _print(
+            len(repos),
+            "dirty",
+            "repositories" if len(repos) > 2 else "repository",
+            "found:",
+        )
+        for r in repos:
+            _print()
+            _print("*", r.toplevel, "@", r.revision)
+            for l in r.status.splitlines():
+                _print("|  ", l)
+
+        _print()
+        _print("Aborting...", end="")
+        return io.getvalue()
+
+
+def recrepo(pointers, ignore_dirty):
+    repos = list(map(repostate, pointers))
+    if not ignore_dirty and not all(r.is_clean for r in repos):
+        raise DirtyRepositories(repos)
+    return list(map(to_record, repos))
+
+
+def cli(output, **kwargs):
+    try:
+        records = recrepo(**kwargs)
+        with output as file:
+            json.dump(records, file, sort_keys=True)
+    except DirtyRepositories as err:
+        print(err, file=sys.stderr)
+        sys.exit(err.CODE)
+
+
+def main(args=None):
+    import argparse
+
+    class CustomFormatter(
+        argparse.RawDescriptionHelpFormatter, argparse.ArgumentDefaultsHelpFormatter
+    ):
+        pass
+
+    parser = argparse.ArgumentParser(
+        formatter_class=CustomFormatter, description=__doc__
+    )
+
+    parser.add_argument(
+        "--ignore-dirty",
+        default=False,
+        action="store_true",
+        help="""
+        Ignore repositories with un-committed files.  By default,
+        un-committed files result in exit with return code {code}.
+        """.format(
+            code=DirtyRepositories.CODE
+        ),
+    )
+
+    parser.add_argument(
+        "--output",
+        default="-",
+        type=argparse.FileType("w"),
+        help="""
+        Path to output JSON file.  "-" means stdout.
+        """,
+    )
+
+    parser.add_argument(
+        "pointers",
+        metavar="pointer",
+        nargs="+",
+        help="""
+        Path to a file or directory inside a project.  Multiple paths
+        can be given.
+        """,
+    )
+
+    ns = parser.parse_args(args)
+    cli(**vars(ns))
+
+
+if __name__ == "__main__":
+    main()
